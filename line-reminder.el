03db139029228ddbdffc9429b442d050dbf9378b
jenchieh <jcs090218@gmail.com>
2020-07-12 01:10:06 +0800

    Bump version.

--- line-reminder.el
+++ line-reminder.el
@@ -6,7 +6,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Description: Line annotation similar to Visual Studio.
 ;; Keyword: annotation line number linum reminder
-;; Version: 0.3.5
+;; Version: 0.3.6
 ;; Package-Requires: ((emacs "24.4"))
 ;; URL: https://github.com/jcs090218/line-reminder
 
