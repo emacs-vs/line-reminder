45468c583826e5e4a96fbd42e9af7e08aa6d25e4
jenchieh <jcs090218@gmail.com>
2020-07-11 21:14:05 +0800

    Added keywords.

--- line-reminder.el
+++ line-reminder.el
@@ -5,7 +5,7 @@
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Description: Line annotation similar to Visual Studio.
-;; Keyword: annotation linum reminder
+;; Keyword: annotation line number linum reminder
 ;; Version: 0.3.4
 ;; Package-Requires: ((emacs "24.4"))
 ;; URL: https://github.com/jcs090218/line-reminder
